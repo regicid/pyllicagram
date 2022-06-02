338eba324535a45ec80d90499b8359fcc6d0778c
regicid <57593826+regicid@users.noreply.github.com>
2022-06-02 13:00:22 +0200

    Ajout de la résolution

--- pyllicagram.py
+++ pyllicagram.py
@@ -4,5 +4,9 @@ def pyllicagram(gram,corpus="presse",debut=1789,fin=1950,resolution="default"):
 	assert corpus in ["lemonde","livres","presse"], 'Vous devez choisir le corpus parmi "lemonde","livres" et "presse"'
 	assert resolution in ["default","annee","mois"], 'Vous devez choisir la résolution parmi "default", "annee" ou "mois"'
 	df = pd.read_csv(f"https://shiny.ens-paris-saclay.fr/guni/corpus={corpus}_{gram}_from={debut}_to={fin}")
+	if resolution=="mois" and corpus != "livres":
+		df = df.groupby(["annee","mois","gram"]).agg({'n':'sum','total':'sum'}).reset_index()
+	if resolution=="annee":
+		df = df.groupby(["annee","gram"]).agg({'n':'sum','total':'sum'}).reset_index()
 	df["ratio"] = df.n.values/df.total.values
 	return df
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyllicagram',
+    version='1.0',
+    license='Creative Commons',
+    author="Benoit de Courson",
+    author_email='b.decourson@csl.mpg.de',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/regicid/pyllicagram',
+    keywords='example project',
+    install_requires=[
+          'pandas',
+      ],
+
+)
